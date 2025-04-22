e0767973c66461ccc82cfb816ed4c3c6e04f7939
rajputshashank003 <hariomshashank@gmail.com>
2025-04-22 20:10:24 +0530

    change backend api link

--- client/src/utils/backend.ts
+++ client/src/utils/backend.ts
@@ -1,3 +1,3 @@
 export const backend_local = 'http://localhost:8000';
-export const backend_vercel = "https://dsa-tutors-api.vercel.app";
+export const backend_vercel = "https://ratsi-api.vercel.app";
 export const backend = backend_vercel;
